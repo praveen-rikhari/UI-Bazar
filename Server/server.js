f0d20589ad063946ac9b8f88ecc93be7042c77e9
praveen-rikhari <praveenrikhari45@gmail.com>
2024-07-21 19:54:16 +0530

    changes in server folder

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { ClerkExpressWithAuth } = require('@clerk/clerk-sdk-node');
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(ClerkExpressWithAuth());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
