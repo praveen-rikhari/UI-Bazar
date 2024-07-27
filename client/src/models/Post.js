75796fc9dfce36f421eafb64180170275cf9d925
praveen-rikhari <praveenrikhari45@gmail.com>
2024-07-27 22:47:17 +0530

    Created Post model

---
+++ client/src/models/Post.js
@@ -0,0 +1,14 @@
+import mongoose, { model, models, Schema } from 'mongoose';
+
+const postSchema = new Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true },
+    description: { type: String },
+    category: { type: String, required: true },
+    htmlCode: { type: String, required: true },
+    cssCode: { type: String, required: true },
+    likesCount: { type: Number, default: 0 }
+});
+
+const Post = models.Post || model("Post" , postSchema);
+export default Post;
