ca09c1645f2c9d58c23ca91d28598cdfe784f470
Suryadev <suryadev@pop-os.localdomain>
2024-07-28 00:21:32 +0530

    changed post api route

--- client/src/app/api/post/route.js
+++ client/src/app/api/post/route.js
@@ -5,15 +5,10 @@ import Post from "@/models/Post";
 export async function POST(req) {
     try {
         dbConnect();
-        const post = new Post({
-            userId,
-            title,
-            description,
-            category,
-            htmlCode,
-            cssCode,
-        });
+        const postData = await req.json();
+        const post = new Post(postData);
         await post.save();
+        return NextResponse.json("post created");
     } catch (error) {
         console.log(error);
         return NextResponse.json("Error in creating the post.........");
