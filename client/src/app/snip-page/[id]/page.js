ad2755a52db3863e263e542f2a08854151861a07
praveen-rikhari <praveenrikhari45@gmail.com>
2024-07-29 23:44:15 +0530

    Added single snippet page

--- client/src/app/snip-page/[id]/page.js
+++ client/src/app/snip-page/[id]/page.js
@@ -1,6 +1,77 @@
+"use client"
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './SnipPage.css'
+
 const SnipPage = ({ params }) => {
+    const [posts, setPosts] = useState({});
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get(`/api/post/${params.id}`);
+                if (response) {
+                    setPosts(response.data);
+                    console.log(response.data);
+                }
+
+            } catch (error) {
+                console.error("Error while fetching single post : ", error);
+            }
+        }
+        fetchPosts();
+    }, [])
+
+    const createIframeContent = (htmlCode, cssCode) => {
+        const iframeDocument = `
+                                <html>
+                                <head>
+                                    <style>
+                                        body {
+                                            display: flex;
+                                            justify-content: center;
+                                            align-items: center;
+                                            height: 100vh;
+                                            margin: 0;
+                                        }
+                                        ${cssCode}
+                                    </style>
+                                </head>
+                                <body>
+                                    ${htmlCode}
+                                </body>
+                            </html>
+        `;
+        return iframeDocument;
+    };
     return (
-        <div>hello this is the snip page of post id {params.id}</div>
+        <div className="singlePost-page">
+            <div className="post-container">
+                {
+                    <div className='post-card'>
+                        <h3>
+                            {posts.name}
+                        </h3>
+                        <iframe
+                            srcDoc={createIframeContent(posts.htmlCode, posts.cssCode)}
+                            className="code-preview"
+                            title={`Post ${posts._id}`}
+                            sandbox="allow-scripts allow-same-origin"
+                        />
+                        <div className='htmlCode-box'>
+                            <code>
+                                {posts.htmlCode}
+                            </code>
+                        </div>
+                        <div className='cssCode-box'>
+                            <code>
+                                {posts.cssCode}
+                            </code>
+                        </div>
+                    </div>
+                }
+            </div>
+        </div>
     )
 }
 
