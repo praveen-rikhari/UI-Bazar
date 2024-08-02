ace6f863efee3951f4cc5ccfeeeb6d8014547889
praveen-rikhari <praveenrikhari45@gmail.com>
2024-08-02 21:19:24 +0530

    function for formatting date

--- client/src/app/snip-page/[id]/page.js
+++ client/src/app/snip-page/[id]/page.js
@@ -84,6 +84,19 @@ const SnipPage = ({ params }) => {
         return iframeDocument;
     };
 
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        return date.toLocaleString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        });
+    }
+
+
     return (
         <div className="singlePost-page">
             <div className="post-container">
