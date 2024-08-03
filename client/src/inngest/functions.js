b23a7a3963a518272511229d82d1cee7330a4a5b
praveen-rikhari <praveenrikhari45@gmail.com>
2024-08-03 17:44:06 +0530

    Adding new key to User Collection

--- client/src/inngest/functions.js
+++ client/src/inngest/functions.js
@@ -8,7 +8,7 @@ export const syncUser = inngest.createFunction(
     { event: 'clerk/user.created' }, // ← This is the function's triggering event
     async ({ event }) => {
         const user = event.data; // The event payload's data will be the Clerk User json object
-        const { id, first_name, last_name } = user;
+        const { id, first_name, last_name, image_url } = user;
         const email = user.email_addresses.find(e =>
             e.id === user.primary_email_address_id
         ).email
@@ -19,7 +19,8 @@ export const syncUser = inngest.createFunction(
                 clerk_user_id: id,
                 first_name,
                 last_name,
-                email
+                email,
+                imgUrl: image_url,
             });
             await newUser.save();
         } catch (error) {
--- client/src/models/User.js
+++ client/src/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
     first_name: String,
     last_name: String,
     email: String,
+    imgUrl: String,
 }, {
     timestamps: true,
 });
