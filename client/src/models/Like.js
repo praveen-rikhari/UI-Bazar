83450c03af7e641583601ca37b9fa96cfead62b7
praveen-rikhari <praveenrikhari45@gmail.com>
2024-08-04 19:34:22 +0530

    Adding Like Model

---
+++ client/src/models/Like.js
@@ -0,0 +1,8 @@
+import mongoose, { model, models, Schema } from 'mongoose';
+
+const likeSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+});
+
+const Like = models.Like || model("Like", likeSchema);
