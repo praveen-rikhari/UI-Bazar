ad5e81210ebcf2313a587c714c65c3e11b2d4ebb
praveen-rikhari <praveenrikhari45@gmail.com>
2024-08-23 20:52:17 +0530

    updating single snip page

--- client/src/app/snip-page/[id]/page.js
+++ client/src/app/snip-page/[id]/page.js
@@ -40,7 +40,7 @@ const SnipPage = ({ params }) => {
 
     // fucntion to add comment
     const addComment = (newComment) => {
-        setAllComments((prevComments) => [ newComment,...prevComments]);
+        setAllComments((prevComments) => [newComment, ...prevComments]);
     };
 
     // function to delete comment
@@ -68,7 +68,7 @@ const SnipPage = ({ params }) => {
         const fetchComments = async () => {
             try {
                 const response = await axios.get(`/api/comment/${posts._id}`);
-                        // Reverse the order of comments to show the latest first
+                // Reverse the order of comments to show the latest first
                 setAllComments(response.data.reverse());
             } catch (error) {
                 console.error("Error fetching comments:", error);
@@ -115,9 +115,20 @@ const SnipPage = ({ params }) => {
                             </span>
                         </span>
                         <div className="post-actions">
-                            <button>Edit</button>
-                            {posts.userId === userId && <Link href="/browse">
-                                <button onClick={() => handlePostDelete(params.id)}>Delete</button></Link>
+                            {
+                                posts.userId === userId && (
+                                    <Link href={`/edit/${params.id}`}>
+                                        <button>Edit</button>
+                                    </Link>
+                                )
+                            }
+
+                            {
+                                posts.userId === userId && (
+                                    <Link href="/browse">
+                                        <button onClick={() => handlePostDelete(params.id)}>Delete</button>
+                                    </Link>
+                                )
                             }
                         </div>
                     </h3>
